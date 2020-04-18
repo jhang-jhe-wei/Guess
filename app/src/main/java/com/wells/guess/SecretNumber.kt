b70d32b8977bc004144093a4254e98efe79acaa1
wells <jhang0912407249@gmail.com>
2020-04-18 15:48:27 +0800

    function design,toast,dialog

---
+++ app/src/main/java/com/wells/guess/SecretNumber.kt
@@ -0,0 +1,18 @@
+package com.wells.guess
+
+import java.util.*
+
+
+class SecretNumber {
+    val secret:Int= Random().nextInt(10)+1
+    var count = 0
+    fun validate(number:Int) :Int{
+        count++
+        return number - secret
+    }
+}
+fun main(){
+    val secretNumber=SecretNumber()
+    println(secretNumber.secret);
+    println("${secretNumber.validate(2)},count：${secretNumber.count}")
+}
